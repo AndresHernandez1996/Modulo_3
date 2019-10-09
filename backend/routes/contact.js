b75188e6c5d8b32d799b8be1607fd5a9b2a78ecf
Yayo Del Rey <yayodelrey@MacBook-Pro-de-Andres.local>
2019-10-08 22:35:34 -0500

    Minor changes for QrCodeEmergency

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -4,13 +4,18 @@ const Contact = require('../models/Contact')
 const User = require('../models/User')
 
 router.post('/create-contact', (req, res, next) => {
-  // console.log('>>>>>>>>>>>>vengo del back', req.body)
   Contact.create({ ...req.body.contact, parent: [req.user.id] })
     .then(contact => res.status(201).json({ contact }))
     .catch(err => res.status(500).json({ err }))
 })
 
 router.get('/contacts', (req, res, next) => {
+  Contact.find()
+    .then(contact => res.status(201).json({ contact }))
+    .catch(err => res.status(500).json({ err }))
+})
+
+router.get('/qrCodeEmergency/:id', (req, res, next) => {
   Contact.find()
     .then(contact => res.status(201).json({ contact }))
     .catch(err => res.status(500).json({ err }))
